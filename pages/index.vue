bd831b5e9a65cd3a08d6a4327db1e68c05eaaa7c
ReiSogawa <natto.and.eggs@gmail.com>
2020-11-21 17:06:54 +0900

    add: home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,25 @@
 <template>
-  <b-container>
+  <b-container class="home">
     <TheHeader class="my-3" />
     <h1 class="text-center">Hiden no Tare</h1>
+    <b-list-group>
+      <b-list-group-item v-for="note in notes" :key="note.note_id">
+        {{ note.title }}
+      </b-list-group-item>
+    </b-list-group>
   </b-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import TheHeader from '@/components/TheHeader.vue'
-import { mapActions } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 
 export default Vue.extend({
   components: { TheHeader },
+  computed: {
+    ...mapGetters('home', ['notes']),
+  },
   created() {
     this.fetchNotes()
   },
@@ -20,3 +28,9 @@ export default Vue.extend({
   },
 })
 </script>
+
+<style scoped>
+.home {
+  max-width: 720px;
+}
+</style>
--- store/home/actions.js
+++ store/home/actions.js
@@ -6,20 +6,21 @@ const noteHistoriesRef = db.collection('note_histories')
 export default {
   async getNotes() {
     const noteDocs = (await notesRef.orderBy('created_at', 'desc').get()).docs
-    const getNoteHistoryDocsArray = noteDocs.map((noteDoc) =>
+    const noteHistoryQueries = noteDocs.map((noteDoc) =>
       noteHistoriesRef
         .where('note_id', '==', noteDoc.id)
         .orderBy('created_at', 'desc')
         .limit(1)
         .get()
     )
-    const noteHistoryDocsArray = await Promise.all(getNoteHistoryDocsArray)
-    return noteHistoryDocsArray.map((noteHistoryDocs) =>
-      noteHistoryDocs.docs[0].data()
+    const noteHistorySnapshots = await Promise.all(noteHistoryQueries)
+    const notes = noteHistorySnapshots.map((snapshot) =>
+      snapshot.docs[0].data()
     )
+    return notes
   },
   async fetchNotes(context) {
     const notes = await context.dispatch('getNotes')
-    console.log(notes)
+    context.commit('SET_NOTES', notes)
   },
 }
