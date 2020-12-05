0fe3957042da32821a0ea8a4b5f19ab680e44c0e
ReiSogawa <natto.and.eggs@gmail.com>
2020-12-05 13:24:15 +0900

    auth 関連の機能を実装。decorator を使った。

---
+++ components/TheHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-navbar>
+    <b-container>
+      <b-navbar-nav>
+        <template v-if="isSignedIn">
+          <b-nav-item @click="onSignOutClick">Sign Out</b-nav-item>
+        </template>
+        <b-nav-item v-else @click="onSignInClick">Sign In</b-nav-item>
+      </b-navbar-nav>
+    </b-container>
+  </b-navbar>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import { authStore } from '@/store'
+
+@Component
+export default class TheHeader extends Vue {
+  get isSignedIn() {
+    return authStore.isSignedIn
+  }
+
+  onSignInClick() {
+    authStore.signIn()
+  }
+
+  onSignOutClick() {
+    authStore.signOut()
+  }
+}
+</script>
---
+++ plugins/firebaseApp.ts
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: 'AIzaSyCKEcyR-Zk8ygv1oF_0E9L0vuLRO9kv2qc',
+    authDomain: 'hiden-no-tare.firebaseapp.com',
+    databaseURL: 'https://hiden-no-tare.firebaseio.com',
+    projectId: 'hiden-no-tare',
+    storageBucket: 'hiden-no-tare.appspot.com',
+    messagingSenderId: '656427067087',
+    appId: '1:656427067087:web:1bb2e46ba8ce48141a79ba',
+    measurementId: 'G-7YEQNDQGXX',
+  })
+}
+
+export const Timestamp = firebase.firestore.Timestamp
+export const FieldValue = firebase.firestore.FieldValue
+export const db = firebase.firestore()
+export const auth = firebase.auth()
+export const GoogleAuthProvider = new firebase.auth.GoogleAuthProvider()
+export default firebase.app()
